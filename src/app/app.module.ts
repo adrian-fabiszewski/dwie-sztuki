19e750e1ea284e63fa527c1c2ac853b9565ef755
Adrian Fabiszewski <adrian.fabiszewski@outlook.com>
2020-05-05 17:58:16 +0200

    Navigation bar test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CastComponent } from './cast/cast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    ContactComponent,
+    CastComponent
   ],
   imports: [
     BrowserModule,
